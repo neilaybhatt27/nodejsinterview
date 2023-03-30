ebb44ef5d537ae9cd7bb0206e4669a031453c815
neilaybhatt27 <neilaybhatt27@gmail.com>
2023-03-30 23:51:03 +0530

    task-1 with simple caching done

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const express = require('express');
 
 const app = express();
+const cache = {}
 
 app.get('/task-1', (req,res) => {
     const number = req.query.number;
-    const result = [];
 
-    for(let i=1; i<=number; i++){
-        if(i%3 === 0 && i%5 === 0){
-            result.push('FizBuz');
-        }
-        else if(i%3 === 0){
-            result.push('Fiz');
-        }
-        else if(i%5 === 0){
-            result.push('Buz');
-        }
-        else{
-            result.push(i);
-        }
+    if(cache[number]){
+        res.send(cache[number]);
     }
+    else{
+        const arr = [];
+        for(let i=1; i<=number; i++){
+            if(i%3 === 0 && i%5 === 0){
+                arr.push('FizBuz');
+            }
+            else if(i%3 === 0){
+                arr.push('Fiz');
+            }
+            else if(i%5 === 0){
+                arr.push('Buz');
+            }
+            else{
+                arr.push(i);
+            }
+        }
+        const msg = arr.join(' ');
+        cache[number] = msg;
+        res.send(msg);
 
-    res.send(result.join(' '));
+    }
 });
 
 app.listen(3000, () => {
